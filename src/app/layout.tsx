57b4a0456691c542868a72641c26a32ff13ecd74
subki <subki@pinjamduit.co.id>
2026-03-17 22:39:48 +0700

    feat: adding view tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import Navbar from '@/components/Navbar'
+import Script from 'next/script'
 
 export const metadata = { title: process.env.NEXT_PUBLIC_APP_URL || 'MyApp' }
 
@@ -9,6 +10,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <Navbar />
         <main>{children}</main>
+        <Script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "3e9d5c5d7ee34739839d3795a0fccc1c"}'
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   )
